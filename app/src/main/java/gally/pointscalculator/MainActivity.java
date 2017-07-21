b32662555918f893f5832d51101f12c8ba0a5400
Steve Martin <c15446768@mydit.ie>
2017-07-21 23:03:38 +0100

    Backend started

--- app/src/main/java/gally/pointscalculator/MainActivity.java
+++ app/src/main/java/gally/pointscalculator/MainActivity.java
@@ -2,13 +2,63 @@ package gally.pointscalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int[] higher = {100, 88, 77, 66, 56, 46, 37, 0};
+    final int[] ordinary = {56, 46, 37, 28, 20, 12, 0, 0};
+    int[] user_six = {1, 4, 776, 34, 2 ,34, 6};
+    int points = 0;
+    int subjects_entered = 0;
+    int previous = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        update();
+        Log.e("Hello", "lmao");
+    }
+
+    public void update()
+    {
+        String test = Integer.toString(higher[2]);
+        Log.d("swag", test);
+        sort();
+    }
+
+    public void sort()
+    {
+        int temp;
+        if (user_six == null)
+        {
+            return;
+        }
+        else
+        {
+            for (int i = 1; i < user_six.length; i++)
+            {
+                for (int j = 0; j < user_six.length - 1; j++)
+                if (user_six[j] < user_six[j + 1])
+                {
+                    temp = user_six[j];
+                    user_six[j] = user_six[j + 1];
+                    user_six[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < 6; i ++) {
+            Log.e("users six", Integer.toString(user_six[i]));
+        }
+    }
+
+    public void reset()
+    {
+        user_six = null;
+        points = 0;
+        subjects_entered = 0;
+        previous = 0;
     }
 
 
