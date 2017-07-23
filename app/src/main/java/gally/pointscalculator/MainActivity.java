b76a84cdaa66d0252aaa3173d541e2c8ed4e8f90
Jack Gallagher <jackgallagher7@gmail.com>
2017-07-23 16:29:07 +0100

    poop

--- app/src/main/java/gally/pointscalculator/MainActivity.java
+++ app/src/main/java/gally/pointscalculator/MainActivity.java
@@ -1,44 +1,79 @@
 package gally.pointscalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     final int[] higher = {100, 88, 77, 66, 56, 46, 37, 0};
     final int[] ordinary = {56, 46, 37, 28, 20, 12, 0, 0};
-    int[] user_six = {1, 4, 776, 34, 2 ,71, 6};
+    int[] user_six = {0, 0, 0, 0, 0, 0, 0};
     int points = 0;
     int subjects_entered = 0;
     int previous = 0;
+    TextView points_view;
+    TextView subjects_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        update();
-        Log.e("HPoints", Integer.toString(points));
+//        update();
+//        Log.e("HPoints", Integer.toString(points));
     }
 
-    public void update()
-    {
-        String test = Integer.toString(higher[2]);
-        Log.d("swag", test);
-        calculate(21);
+    public void setViews() {
+
+        points_view = (TextView) findViewById(R.id.Points);
+        points_view.setText(Integer.toString(points));
+
+        subjects_view = (TextView) findViewById(R.id.subjects);
+        subjects_view.setText("Number of Subjects entered: " + Integer.toString(subjects_entered));
     }
+
+//    public void update()
+//    {
+//        calculate(21);
+//    }
 
     public void calculate(int value)
     {
-        user_six[subjects_entered] = value;
-        previous = value;
-        subjects_entered++;
-        sort();
-        for (int i = 0; i < 6; i++)
-        {
-            points += user_six[i];
+        if(user_six[5] < value && subjects_entered < 6) {
+
+            user_six[subjects_entered] = value;
+            previous = value;
+            subjects_entered++;
+            sort();
+            points = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                points += user_six[i];
+            }
+        }
+        else if(user_six[5] < value){
+
+            user_six[5] = value;
+            previous = value;
+            subjects_entered++;
+            sort();
+            points = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                points += user_six[i];
+            }
+        }
+        else {
+            subjects_entered++; //to ensure subjects entered counter continues increasing
         }
     }
 
@@ -69,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void reset()
     {
-        user_six = null;
+        Arrays.fill(user_six, 0);
         points = 0;
         subjects_entered = 0;
         previous = 0;
@@ -77,12 +112,86 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonTap(View v) {
 
-        if(v.getId() == R.id.H1) {
+        switch(v.getId()) {
 
-            Toast myToast = Toast.makeText(getApplicationContext(), Integer.toString(higher[0]), Toast.LENGTH_SHORT);
-            myToast.show();
+            case R.id.H1:
+                calculate(higher[0]);
+                setViews();
+                break;
+            case R.id.H2:
+                calculate(higher[1]);
+                setViews();
+                break;
+            case R.id.H3:
+                calculate(higher[2]);
+                setViews();
+                break;
+            case R.id.H4:
+                calculate(higher[3]);
+                setViews();
+                break;
+            case R.id.H5:
+                calculate(higher[4]);
+                setViews();
+                break;
+            case R.id.H6:
+                calculate(higher[5]);
+                setViews();
+                break;
+            case R.id.H7:
+                calculate(higher[6]);
+                setViews();
+                break;
+            case R.id.H8:
+                calculate(higher[7]);
+                setViews();
+                break;
+            case R.id.O1:
+                calculate(ordinary[0]);
+                setViews();
+                break;
+            case R.id.O2:
+                calculate(ordinary[1]);
+                setViews();
+                break;
+            case R.id.O3:
+                calculate(ordinary[2]);
+                setViews();
+                break;
+            case R.id.O4:
+                calculate(ordinary[3]);
+                setViews();
+                break;
+            case R.id.O5:
+                calculate(ordinary[4]);
+                setViews();
+                break;
+            case R.id.O6:
+                calculate(ordinary[5]);
+                setViews();
+                break;
+            case R.id.O7:
+                calculate(ordinary[6]);
+                setViews();
+                break;
+            case R.id.O8:
+                calculate(ordinary[7]);
+                setViews();
+                break;
+            case R.id.HL:
+                setContentView(R.layout.activity_ordinary);
+                setViews();
+                break;
+            case R.id.OL:
+                setContentView(R.layout.activity_main);
+                setViews();
+                break;
+            case R.id.reset:
+                reset();
+                setViews();
+                Toast myToast = Toast.makeText(getApplicationContext(), "Reset!", Toast.LENGTH_SHORT);
+                myToast.show();
+                break;
         }
     }
-
-
 }
